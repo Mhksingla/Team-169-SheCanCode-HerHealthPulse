399373234fa0a48aced84b366de6d56a0d168722
Mhksingla <156797121+Mhksingla@users.noreply.github.com>
2025-10-08 03:30:11 +0530

    Update vite.config.js

--- hackwithher/vite.config.js
+++ hackwithher/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// ✅ Working config for Tailwind + React + Vite
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // Important for Vercel
+  plugins: [react(),tailwindcss()],
 })
